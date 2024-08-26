004413fc0fcd3ea46df08c9109b80d4193da8126
SergioLGaytan <sergiolopgaytan@gmail.com>
2024-08-25 19:26:26 -0600

    feat(*): create button component

--- src/ButtonDefault.js
+++ src/ButtonDefault.js
@@ -7,27 +7,49 @@ export class ButtonDefault extends LitElement {
       padding: 25px;
       color: var(--button-default-text-color, #000);
     }
+
+    button {
+      border-radius: 8px;
+      border: 1px solid transparent;
+      padding: 9px 18px;
+      font-size: 16px;
+      font-weight: 500;
+      background: #1a1a1a;
+      cursor: pointer;
+      transition: border-color 0.25s;
+      color: #f9f9f9;
+    }
+
+    button:hover {
+      border-color: #646cff;
+    }
   `;
 
   static properties = {
-    header: { type: String },
-    counter: { type: Number },
+    customName: { type: String, attribute:'custom-name' },
+    buttonText: { type: String, attribute:'button-text'}
   };
 
   constructor() {
     super();
-    this.header = 'Hey there';
-    this.counter = 5;
+    this.customName = 'button-click';
+    this.buttonText = 'Increment';
   }
 
-  __increment() {
-    this.counter += 1;
-  }
+  _customEvent(eventName, detail) {
+    this.dispatchEvent(new CustomEvent(eventName, {
+      composed: true,
+      bubbles: true,
+      detail
+    }));
+  };
 
   render() {
     return html`
-      <h2>${this.header} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <button 
+      @click=${() => this._customEvent(this.customName, true)}>
+        ${this.buttonText}
+      </button>
     `;
   }
 }
